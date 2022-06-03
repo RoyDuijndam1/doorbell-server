9e54e88570110805a627a37162a15b9d35b2a4f6
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2022-06-03 13:09:09 +0200

    Fix HardwareListener

--- src/main/java/net/duijndam/doorbell/server/HardwareListener.java
+++ src/main/java/net/duijndam/doorbell/server/HardwareListener.java
@@ -7,12 +7,14 @@ import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
 import com.pi4j.io.gpio.digital.DigitalStateChangeListener;
 import com.pi4j.io.gpio.digital.PullResistance;
 
+
 abstract public class HardwareListener {
     private static final Integer PIN_BUTTON = 12;
-    private final Context pi4j = Pi4J.newContext();
+    private final Context pi4j = Pi4J.newAutoContext();
     protected DigitalInput button;
 
     public HardwareListener() {
+        var context = Pi4J.newContext();
         var buttonBuilder = this.createButtonBuilder();
         this.setup(buttonBuilder);
         this.button = pi4j.create(buttonBuilder);
@@ -29,6 +31,7 @@ abstract public class HardwareListener {
                 .address(PIN_BUTTON)
                 .pull(PullResistance.PULL_UP)
                 .debounce(3000L);
+
     }
 
     abstract void setup(DigitalInputConfigBuilder buttonBuilder);
